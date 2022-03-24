8be39931cbc6af76af00b3318fdd137678f3ad70
vobaloi <vobaloi2k@gmail.com>
2022-03-24 16:57:40 +0700

    delete frontend

--- FrontEnd/client/src/components/ManagementUsers/ManagementsUsers.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ManagementsUsers = () => {
-    return (
-        <div>ManagementsUsers</div>
-    )
-}
-
-export default ManagementsUsers
--- FrontEnd/client/src/components/SettingProfiles/SettingProfiles.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SettingProfiles = () => {
-    return (
-        <div>SettingProfiles</div>
-    )
-}
-
-export default SettingProfiles
