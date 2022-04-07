3b87fd2427f3d00a0f4f1ddc5918381c34ac6724
nhanle03082000 <nhanlt03082000@gmail.com>
2022-04-07 19:46:21 +0700

    new

--- back _end/models/category.model.js
+++ back _end/models/category.model.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
-  Deapart_id: { type: mongoose.Schema.Types.ObjectId, ref: "Department" },
+  departs: { type: mongoose.Schema.Types.ObjectId, ref: "Department" },
   name_category: String,
-  start_day: Date,
-  end_day: Date,
   description: String,
+  name_depart: String,
 });
 
 const Category = mongoose.model("Category", schema);
