2bdb0c3f701765e8923b397bff87e4827ba83644
Phuuoc <phuocntgcc18019@fpt.edu.vn>
2022-04-08 15:45:42 +0700

    new

--- back _end/server.js
+++ back _end/server.js
@@ -40,7 +40,7 @@ require("./routes/index")(app);
 
 const http = require("http");
 const server = http.Server(app);
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`server is running on port localhost:${port}`);
 });
